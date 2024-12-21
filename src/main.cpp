3a645f844c7790002631e23fcfd736c110a611e7
Andy <andres.meneguzzo@gmail.com>
2024-12-21 21:42:06 +0100

    Cleaned up the main function, code works correctly. TODO: Add more features to the projects

--- src/main.cpp
+++ src/main.cpp
@@ -3,42 +3,6 @@
 
 #include <iostream>
 
-/*
-class Contact {
-public:
-    Contact(const std::string& name, const std::string& phone, const std::string& email)
-        : name(name), phone(phone), email(email) {}
-
-    void display() const {
-        std::cout << "Name: " << name << "\nPhone: " << phone << "\nEmail: " << email << std::endl;
-    }
-
-private:
-    std::string name;
-    std::string phone;
-    std::string email;
-};
-*/
-
-/*
-class ContactManager {
-public:
-    void addContact(const std::string& name, const std::string& phone, const std::string& email) {
-        contacts.emplace_back(name, phone, email);
-    }
-
-    void displayContacts() const {
-        for (const auto& contact : contacts) {
-            contact.display();
-            std::cout << "-------------------" << std::endl;
-        }
-    }
-
-private:
-    std::vector<Contact> contacts;
-};
-*/
-
 int main() {
     ContactManager manager;
 
