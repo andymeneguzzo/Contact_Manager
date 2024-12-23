0cb16acc3b2e03785e94d956d7bf916206d25dc6
Andy <andres.meneguzzo@gmail.com>
2024-12-23 22:25:59 +0100

    Added statistics for category and group attributes

--- src/Statistics.cpp
+++ src/Statistics.cpp
@@ -6,11 +6,15 @@ void Statistics::generateStatistics(const std::vector<Contact>& contacts) const 
     std::map<std::string, int> genderStats;
     std::map<std::string, int> statusStats;
     std::map<std::string, int> professionStats;
+    std::map<std::string, int> categoryStats;
+    std::map<std::string, int> groupStats;
 
     for (const auto& contact : contacts) {
         genderStats[contact.getGender()]++;
         statusStats[contact.getStatus()]++;
         professionStats[contact.getProfession()]++;
+        categoryStats[contact.getCategory()]++;
+        groupStats[contact.getGroup()]++;
     }
 
     std::cout << "\n=== Statistiche ===\n";
@@ -28,5 +32,15 @@ void Statistics::generateStatistics(const std::vector<Contact>& contacts) const 
     std::cout << "\nProfessione:\n";
     for (const auto& [profession, count] : professionStats) {
         std::cout << profession << ": " << count << "\n";
+    }
+
+    std::cout << "\nCategoria:\n";
+    for (const auto& [category, count] : categoryStats) {
+        std::cout << category << ": " << count << "\n";
+    }
+
+    std::cout << "\nGruppo:\n";
+    for (const auto& [group, count] : groupStats) {
+        std::cout << group << ": " << count << "\n";
     }
 }
